ba5819d555833fbd6d9795462d2ac6d479a1f9fc
vmakar9 <zmerin212@gmail.com>
2022-10-15 21:47:27 +0300

    1

--- src/Components/Movies/MoviesList/Movie.js
+++ src/Components/Movies/MoviesList/Movie.js
@@ -1,9 +1,9 @@
 export default function Movie({movie}){
-    const {title,release_date,poster_path}=movie;
+    const {title,release_date}=movie;
     return(<div>
         <div>
             <h3> {title} </h3>
-            <img src={poster_path} alt = 'poster'/>
+
             <h6> {release_date}</h6>
         </div>
     </div>)
